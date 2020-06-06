1848af968cd338bfb29e0bc0d9a5c11221cfd76d
willian-kaminsnki <developer.williansantos@gmail.com>
2020-06-06 18:37:33 -0300

    Add Swagger2 Configuration.

---
+++ src/main/java/com/jpwhealth/configuration/swagger/SwaggerConfiguration.java
@@ -0,0 +1,54 @@
+package com.jpwhealth.configuration.swagger;
+
+import com.google.common.base.Predicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(apis())
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private Predicate<RequestHandler> apis() {
+        return RequestHandlerSelectors.basePackage("com.jpwhealth");
+    }
+
+    private ApiInfo apiInfo() {
+
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title ("JPW-HEALTH-API")
+                .description ("Aplicação que visa auxiliar no combate ao Corona Virús (COVID-19), elaborado durante as aulas do Curso de Análise e Desenvolvimento de Sistemas da Faculdade Senai Porto Alegre.")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .termsOfServiceUrl("/service.html")
+                .version("1.0.0")
+                .contact(new Contact(
+                        "Willian Kaminski",
+                        "https://willian-kaminski.github.io/",
+                        "williankaminski.santos@gmail.com"))
+                .build();
+
+        return apiInfo;
+    }
+
+}
--- src/main/java/com/jpwhealth/controller/FamilyController.java
+++ src/main/java/com/jpwhealth/controller/FamilyController.java
@@ -1,10 +1,12 @@
 package com.jpwhealth.controller;
 
 import com.jpwhealth.domain.Family;
+import com.jpwhealth.domain.dto.FamilyDetailedDto;
 import com.jpwhealth.domain.dto.FamilyDto;
 import com.jpwhealth.domain.form.FamilyForm;
 import com.jpwhealth.domain.form.FamilyFormUpdate;
 import com.jpwhealth.service.FamilyService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,27 +22,32 @@ public class FamilyController {
     @Autowired
     private FamilyService familyService;
 
+    @ApiOperation(value = "Lista o cadastro de familias", notes = "Lista o cadastro de familias necessitadas de auxilio", response = FamilyDto.class, responseContainer = "List" )
     @GetMapping("/family")
     public ResponseEntity<Object> getAll(){
         return ResponseEntity.ok(familyService.getAll());
     }
 
+    @ApiOperation(value = "Lista o cadastro de familias por id", notes = "Lista o cadastro de familias necessitadas de auxilio", response = FamilyDetailedDto.class, responseContainer = "Object" )
     @GetMapping("/family/{id}")
     public ResponseEntity<Object> getById(@PathVariable Long id){
         return ResponseEntity.ok(familyService.getById(id));
     }
 
+    @ApiOperation(value = "Insere uma nova familia", notes = "Insere o cadastro de uma nova familia na nossa base de dados", response = FamilyDto.class, responseContainer = "Object" )
     @PostMapping("/family")
     public ResponseEntity<FamilyDto> register(@Valid @RequestBody FamilyForm familyForm){
         Family family = familyService.register(familyForm);
         return ResponseEntity.status(HttpStatus.CREATED).body(new FamilyDto(family));
     }
 
+    @ApiOperation(value = "Atualiza o cadastro de uma familia", notes = "Atualizados o dados cadastrais da familia", response = FamilyDto.class, responseContainer = "Object" )
     @PutMapping("/family")
     public ResponseEntity<FamilyDto> update(@Valid  @RequestBody FamilyFormUpdate familyFormUpdate){
         return familyService.update(familyFormUpdate);
     }
 
+    @ApiOperation(value = "Remove o cadastro de familia", notes = "Remove o registro cadastral da familia informada", response = ResponseEntity.class, responseContainer = "Object" )
     @DeleteMapping("/family/{id}")
     public ResponseEntity delete(@PathVariable Long id){
         return familyService.delete(id);
