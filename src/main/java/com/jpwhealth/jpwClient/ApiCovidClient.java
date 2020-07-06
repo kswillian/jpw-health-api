d95340179b1dbbb9c9743b7c41146b5a5349597d
willian-kaminsnki <developer.williansantos@gmail.com>
2020-07-05 23:49:49 -0300

    Support in Client Class.

--- src/main/java/com/jpwhealth/jpwClient/ApiCovidClient.java
+++ src/main/java/com/jpwhealth/jpwClient/ApiCovidClient.java
@@ -4,6 +4,7 @@ import com.jpwhealth.domain.CovidGeneralData;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class ApiCovidClient {
@@ -12,12 +13,14 @@ public class ApiCovidClient {
     
     public static CovidGeneralData getCovidGeneralDataTest(){
         
-        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime dateInit = LocalDate.now().atTime(00, 00);
+        LocalDateTime dateFinal = LocalDate.now().atTime(00, 01);
+
         RestTemplate restTemplate = new RestTemplateBuilder()
                 .rootUri(URL_COVID_DATA)
                 .build();
 
-        CovidGeneralData[] covidGeneralData = restTemplate.getForObject("/Brazil?from="+ today +"&to="+ today, CovidGeneralData[].class, "");
+        CovidGeneralData[] covidGeneralData = restTemplate.getForObject("/Brazil?from="+ dateInit +"&to="+ dateFinal, CovidGeneralData[].class, "");
         return covidGeneralData[0];
 
     }
